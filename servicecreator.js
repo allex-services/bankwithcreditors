7b8d78877667fe8d4336852dc80b7d437fe1f195
Andrija Petrovic <andrija.hers@gmail.com>
2015-12-22 07:04:10 +0100

    Lotsa work to make it correct

--- servicecreator.js
+++ servicecreator.js
@@ -22,10 +22,21 @@ function createBankWithCreditorsService(execlib, ParentServicePack) {
     return ParentService.prototype.readAccount.call(this, username);
   };
 
+  var _creditorsuffix = '_creditor',
+    _creditorsuffixlength = _creditorsuffix.length;
+
+  BankWithCreditorsService.prototype.readCreditor = function (username) {
+    return this.readAccountWDefault(username+_creditorsuffix, [0]);
+  };
+
+  BankWithCreditorsService.prototype.chargeCreditor = function(username, amount, referencearry) {
+    return this.superCharge(username+_creditorsuffix, amount, referencearry);
+  };
+
   BankWithCreditorsService.prototype.readAccount = function (username) {
     return q.all([
-      this.superReadAccount(username),
-      this.readAccountWDefault(username+'_creditor', [0])
+      this.readAccountWDefault(username, [0]),
+      this.readCreditor(username)
     ]).spread(
       this.accountSummer.bind(this)
     );
@@ -36,20 +47,25 @@ function createBankWithCreditorsService(execlib, ParentServicePack) {
   };
 
   BankWithCreditorsService.prototype.superCharge = function (username, amount, referencearry) {
-    return ParentService.prototype.superCharge.call(this, username, amount, referencearry);
+    return ParentService.prototype.charge.call(this, username, amount, referencearry);
   };
 
+  BankWithCreditorsService.prototype.readBoth = function (username, cb) {
+    return q.all([
+      this.readAccountWDefault(username, [0]),
+      this.readCreditor(username)
+    ]).spread(cb);
+  };
   BankWithCreditorsService.prototype.charge = function (username, amount, referencearry) {
     try {
-      console.log('charge!, username', username, 'amount', amount);
+      //console.log('charge!, username', username, 'amount', amount);
     if (amount>0) {
       return this.locks.run(username+'--chargecombo--', new qlib.PromiseChainerJob([
-        this.superReadAccount.bind(this, username),
-        this.accountChecker.bind(this, username, amount, referencearry)
+        this.readBoth.bind(this, username, this.accountChecker.bind(this, username, amount, referencearry))
       ]));
     }
     return this.locks.run(username+'--chargecombo--', new qlib.PromiseChainerJob([
-      this.readAccountWDefault.bind(this, username+'_creditor', [0]),
+      this.readCreditor.bind(this, username),
       this.creditorAccountChecker.bind(this, username, amount, referencearry)
     ])); 
     } catch(e) {
@@ -58,38 +74,117 @@ function createBankWithCreditorsService(execlib, ParentServicePack) {
     }
   };
 
-  BankWithCreditorsService.prototype.accountChecker = function (username, amount, referencearry, account) {
-    console.log('accountChecker, username', username, 'amount', amount, 'account', account);
+  BankWithCreditorsService.prototype.accountChecker = function (username, amount, referencearry, account, creditoraccount) {
+    console.log('accountChecker, username', username, 'amount', amount, 'account', account, 'creditoraccount', creditoraccount);
+    var creditorextra, ops;
     if (account[0] < amount) {
-      return q.all([
+      creditorextra = creditoraccount[0] + account[0] - amount;
+      ops = [
         this.superCharge(username, account[0], referencearry),
-        this.superCharge(username+'_creditor', account[0] - amount, referencearry)
-      ]).spread(
-        this.chargeSummer.bind(this)
+      ];
+      if (creditorextra < 0) {
+        ops.push(this.chargeCreditor(username, creditorextra, referencearry));
+      }
+      return q.all(ops).spread(
+        this.outMoneyChargeSummer.bind(this)
       );
     }
     return this.superCharge(username, amount, referencearry);
   };
 
   BankWithCreditorsService.prototype.creditorAccountChecker = function (username, amount, referencearry, creditoraccount) {
-    console.log('creditorAccountChecker, username', username, 'amount', amount, 'account', account);
-    if (creditoraccount[0] < -amount) {
-      return q.all([
-        this.superCharge(username+'_creditor', account[0], referencearry),
-        this.superCharge(username, account[0] + amount, referencearry)
-      ]).spread(
-        this.chargeSummer.bind(this)
-      );
+    try {
+    var creditormoney = creditoraccount[0];
+    if (creditormoney < -amount) {
+      if (creditormoney) {
+        return q.all([
+          this.chargeCreditor(username, creditormoney, referencearry),
+          this.superCharge(username, amount, referencearry)
+        ]).spread(
+          this.inMoneyChargeSummer.bind(this)
+        );
+      } else {
+        return this.superCharge(username, amount, referencearry);
+      }
     }
-    return this.superCharge(username+'_creditor', -amount, referencearry);
+    return q.all([
+      this.chargeCreditor(username, -amount, referencearry),
+      this.superCharge(username, amount, referencearry)
+    ]).spread(
+      this.inMoneyChargeSummer.bind(this)
+    );
+    } catch(e) {
+      console.error(e.stack);
+      console.error(e);
+    }
   };
 
-  BankWithCreditorsService.prototype.chargeSummer = function (mycharge, creditorcharge) {
-    console.log('chargeSummer', mycharge, creditorcharge);
+  BankWithCreditorsService.prototype.outMoneyChargeSummer = function (mycharge, creditorcharge) {
+    console.log('outMoneyChargeSummer', mycharge, creditorcharge);
+  };
+
+  BankWithCreditorsService.prototype.inMoneyChargeSummer = function (creditorcharge, mycharge) {
+    console.log('inMoneyChargeSummer', creditorcharge, mycharge);
   };
   
   BankWithCreditorsService.prototype.__cleanUp = function() {
     ParentService.prototype.__cleanUp.call(this);
+  };
+
+  BankWithCreditorsService.prototype.ensure = function (username, amount, referencearry) {
+    if (amount < 0) {
+      return q.reject(new lib.Error('AMOUNT_FOR_ENSURE_CANNOT_BE_NEGATIVE', amount));
+    }
+    return q.all([
+      this.readAccountWDefault(username, [0]),
+      this.readCreditor(username)
+    ]).spread(
+      this.onAccountForEnsure.bind(this, username, amount, referencearry)
+    );
+  };
+
+  BankWithCreditorsService.prototype.onAccountForEnsure = function (username, amount, referencearry, myaccount, creditoraccount) {
+    try {
+    var mymoney = myaccount[0],
+      creditormoney = creditoraccount[0],
+      totalmoney = mymoney + creditormoney,
+      missing = amount-totalmoney;
+    if (missing > 0) {
+      return this.chargeCreditor(username, -missing, referencearry).then(qlib.returner(amount));
+    } else {
+      return q(amount);
+    }
+    } catch(e) {
+      console.error(e.stack);
+      console.error(e);
+    }
+  };
+
+  BankWithCreditorsService.prototype.estimate = function (username, amountdelta) {
+    return q.all([
+      this.readAccountWDefault(username, [0]),
+      this.readCreditor(username)
+    ]).spread(
+      this.estimator.bind(this, amountdelta)
+    );
+  };
+
+  BankWithCreditorsService.prototype.estimator = function (amountdelta, myaccount, creditoraccount) {
+    var extra = amountdelta - creditoraccount[0];
+    if (extra < 0) {
+      return [creditoraccount[0] + myaccount[0]];
+    }
+    return [creditoraccount[0] + myaccount[0] + extra];
+  };
+
+  BankWithCreditorsService.prototype.isAccount = function (item) {
+    return (item && item.key && item.key.substr && item.key.substr(-_creditorsuffixlength) !== _creditorsuffix);
+  }
+
+  BankWithCreditorsService.prototype.traverseAccounts = function (options) {
+    options = options || {};
+    options.filter = this.isAccount;
+    return this.accounts.traverse(options);
   };
   
   return BankWithCreditorsService;
