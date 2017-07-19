842a6d6ea9555e789a3d162343b3d365e657bc3f
Veljko Popovic <v.s.popovic@gmail.com>
2017-07-19 16:28:01 +0200

    updating dependencies

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function createServicePack(execlib) {
 
   return {
     service: {
-      dependencies: ['allex:bank', 'allex_leveldbbankwithcreditorslib']
+      dependencies: ['allex_bankservice', 'allex_leveldbbankwithcreditorslib']
     },
     sinkmap: {
-      dependencies: ['allex:bank']
+      dependencies: ['allex_bankservice']
     }
   };
 }
